fc1fbb13dd2a48d0630ccf3b0abcd47e09cae00a
ka <ka.kaosf@gmail.com>
2020-02-29 02:02:07 +0900

    Fix JS error `Uncaught TypeError: r is not a function` on production

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.delete('nodeModules')
+// NOTE: Avoid the error on production environment like below:
+//
+//     Uncaught TypeError: r is not a function
+//
+// ref.
+//
+// - https://github.com/rails/rails/issues/35501
+// - https://github.com/rails/webpacker/blob/master/docs/v4-upgrade.md#excluding-node_modules-from-being-transpiled-by-babel-loader
+// - https://github.com/jellypbc/poster/pull/71/files
+
 module.exports = environment
