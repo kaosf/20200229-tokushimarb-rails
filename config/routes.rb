e01e9ab6c1ba1973f494243575477f2863a6b297
ka <ka.kaosf@gmail.com>
2020-02-29 13:57:00 +0900

    bin/rails g controller home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   root to: 'messages#index'
   resources :messages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
