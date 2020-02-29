215955166920f167e41bc63e61ea047b131832cd
ka <ka.kaosf@gmail.com>
2020-02-29 14:20:41 +0900

    Example 1

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+const Hello = props => {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <>
+      <div>Hello {props.name}! {props.age} {number}</div>
+      <button onClick={() => setNumber(number + 1)}>+1</button>
+      <button onClick={() => setNumber(number - 1)}>-1</button>
+    </>
+  );
+}
 
 Hello.defaultProps = {
-  name: 'David'
+  name: 'David',
+  age: 20,
 }
 
 Hello.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+  age: PropTypes.number,
 }
 
 document.addEventListener('DOMContentLoaded', () => {
